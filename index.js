5f02f563adb4537a13290744b9d89ee621bed3dd
Tahmid Dhrubo <86056731+Dhrubo256@users.noreply.github.com>
2021-09-06 12:34:51 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function clickmee(){
+    document.write("<h1 style='color:green;text-align:center;'>Hire করলে লাভ নাই।আমি আইতাম না।</h1>");
+}
+function about(){
+    document.write("<h1 style='color:red;text-align:center;'>তুমি কেমন তা জানিনা but আমি ভদ্র।</h1>");
+}
+function ok(){
+    document.write("<h1 style='color:red;text-align:center;'>Youtube এ গিয়া শিখিছ।আমার কাছে শিখলে কিছু বুঝতি না।</h1>");
+
+}
